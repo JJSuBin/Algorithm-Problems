e00f06391cf163d6949cb79585e6734f23c7af76
정수빈 <45066381+JJSuBin@users.noreply.github.com>
2021-10-24 18:50:33 +0900

    Update BaekJoon_2206.java

--- BaekJoon/BaekJoon_2206.java
+++ BaekJoon/BaekJoon_2206.java
@@ -1,91 +1,90 @@
 import java.util.*;
 
-class Position_2206 {
-	public int x;
-	public int y;
-	public int distance;
-	public int drill; // 공사 횟수
+class Point_2206 {
+	int x, y, count, drill;
 	
-	public Position_2206(int x, int y, int distance, int drill) {
+	public Point_2206(int x, int y, int count, int drill) {
 		this.x=x;
 		this.y=y;
-		this.distance=distance;
-		this.drill=drill; 
+		this.count=count; // 이동 횟수
+		this.drill=drill; // 드릴 사용 횟수
 	}
 }
 
 public class BaekJoon_2206 {
-	public static int[][] map,visited;
-	public static int[] dx= {0,0,-1,1};
-	public static int[] dy= {-1,1,0,0};
-	public static int n,m,answer=Integer.MAX_VALUE;
-	
+	static int n,m,min=Integer.MAX_VALUE;
+	/*
+	 * drill_count 배열은 방문여부 체크와 각 칸에서 드릴의 사용 횟수를 저장하는 배열이다. 
+	 * 배열의 모든 값을 최댓값으로 초기화 한 뒤 탐색하면서 방문 드릴 사용 횟수를 저장하면서 방문 여부까지 체크한다. 
+	 */
+	static int[][] map; 
+	static boolean visited[][][];
+	static int[] dx= {-1,0,1,0};
+	static int[] dy= {0,-1,0,1};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
 		m=sc.nextInt();
-		
 		map=new int[n][m];
-		visited=new int[n][m]; // 해당 좌표의 공사 횟수를 저장하는 배열
+		/*
+		 * 벽을 부수고 도달하는 경로와 벽을 부수지 않고 도달하는 경로의
+		 * 방문처리를 따로 해야하기 때문에 3차원 배열을 사용한다. 
+		 * visited[드릴사용횟수 1or0][x][y]
+		 */
+		visited=new boolean[2][n][m];
 		
 		for(int i=0;i<n;i++) {
 			String str=sc.next();
 			for(int j=0;j<m;j++) {
 				map[i][j]=str.charAt(j)-'0';
-				visited[i][j]=Integer.MAX_VALUE;
-			}
+			}	
 		}
-		
+			
 		bfs(0,0);
-
-		if(answer==Integer.MAX_VALUE)
-			System.out.println(-1);
-		else
-			System.out.println(answer);
+		
+		if(min==Integer.MAX_VALUE) System.out.println(-1);
+		else System.out.println(min);
 	}
 	
-	public static void bfs(int x,int y) {
-		Queue<Position_2206> q=new LinkedList<>();
-		
-		q.offer(new Position_2206(x,y,1,0)); // 시작 지점을 포함한 거리 1로 시작
-		visited[x][y]=0; // 처음 공사 횟수
+	static void bfs(int x, int y) {
+		Queue<Point_2206> q=new LinkedList<>();
+		q.offer(new Point_2206(x,y,1,0)); // 시작 지점을 포함해서 count
+		visited[0][x][y]=true;
 		
 		while(!q.isEmpty()) {
-			Position_2206 position=q.poll();
+			Point_2206 p=q.poll();
 			
-			// 도착지점을 만났다면 이동 거리를 저장하고 종료
-			if(position.y==m-1&&position.x==n-1) {
-				answer=position.distance;
+			// 도착지에 도달한 경우
+			if(p.x==n-1&&p.y==m-1) {
+				min=p.count;
 				break;
 			}
 			
 			for(int i=0;i<4;i++) {
-				int nx=dx[i]+position.x;
-				int ny=dy[i]+position.y;
+				int nx=p.x+dx[i];
+				int ny=p.y+dy[i];
 				
-				// 범위를 벗어난다면 넘어간다.
-				if(ny>=m||nx>=n||nx<0||ny<0)
-					continue;
+				// 배열 범위를 넘어간 경우
+				if(nx<0||ny<0||nx>=n||ny>=m) continue;
 				
-				// 이동한 좌표의 공사 횟수가 기존에 갖고있던 값보다 작을 경우 넘어간다.
-				if(visited[nx][ny]<=position.drill)
-					continue;
-				
-				if(map[nx][ny]==0) { // 벽이 아닐 경우
-					visited[nx][ny]=position.drill; // 부순 횟수 유지
-					// 이동한 좌표와, 이동 거리를 1 증가한 값을 큐에 삽입한다.
-					q.add(new Position_2206(nx,ny,position.distance+1,position.drill));
-				}
-				else { // 벽일 경우
-					if(position.drill==0) { // 지금까지 벽을 부순 횟수가 0일 경우
-						visited[nx][ny]=position.drill+1; // 부순 횟수를 1 높여주고
-						// 이동한 좌표와, 거리와 부순 횟수를 1씩 증가한 값을 큐에 삽입한다.
-						q.add(new Position_2206(nx,ny,position.distance+1,position.drill+1));
+				// 탐색하는 칸이 벽이 아니고
+				if(map[nx][ny]==0) {
+					// 아직 방문하지 않은 곳이라면
+					if(!visited[p.drill][nx][ny]) {
+						visited[p.drill][nx][ny]=true; // 방문처리
+						q.offer(new Point_2206(nx,ny,p.count+1,p.drill));
 					}
 				}
-				
+				// 탐색하는 칸이 벽이고
+				else {
+					// 아직 벽을 뚫지 않았으며, 방문하지 않은 곳이라면
+					if(p.drill==0&&!visited[1][nx][ny]) {
+						visited[1][nx][ny]=true;
+						q.offer(new Point_2206(nx,ny,p.count+1,p.drill+1));
+					}
+				}
 			}
-		}
+		}	
 	}
 }
